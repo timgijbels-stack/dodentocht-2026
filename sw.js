6cd66f3222a69e005061a8fca881e9cc0ae12307
timgijbels-stack <timgijbels@gmail.com>
2026-08-12 11:56:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dodentocht-2026-v13';
+const CACHE='dodentocht-2026-v15';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-v11-180.png','./icon-v11-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
